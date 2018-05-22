8cd8b3be4fe2f5acaffc3128a6e540a415b59804
Benjamin Taylor <staylor.ben@gmail.com>
2018-05-21 22:14:30 -0700

    added shell of a api get request

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+app.get('/api/v1/books', (req, res) => {
+  //some code goes here to get ye books
+})
+
 app.get('*', (req, res) => res.status(403).send('This route does not exist'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
