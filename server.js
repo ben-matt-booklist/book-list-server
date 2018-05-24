70cbd3bb0ca3ed3bdc383181cc0f167828f3a7d0
Benjamin Taylor <staylor.ben@gmail.com>
2018-05-24 11:47:13 -0700

    implemented new book form

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ client.on('error', err => console.log(err));
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
 
 let localUrl = 'http://localhost:8080';
 
-//let deployUrl = 'https://bt-mg-booklist.herokuapp.com'
 let deployUrl = 'https://ben-matt-booklist.github.io/book-list-client/';
 
 
@@ -28,19 +28,32 @@ app.get('/', (req, res) => {
 
 app.get('/api/v1/books', (req, res) => {
   //some code goes here to get ye books
-  let SQL = 'SELECT book_id, title, author, image_url FROM books';
+  let SQL = 'SELECT book_id, title, author, image_url FROM books;';
   client.query(SQL)
     .then(result => res.send(result.rows))
     .catch(console.error);
 })
 
 app.get('/api/v1/books/:id', (req, res) => {
-  let SQL = 'SELECT * FROM books WHERE book_id = $1'
+  let SQL = 'SELECT * FROM books WHERE book_id = $1;'
   let values = [
     req.params.id
   ]
   client.query(SQL, values)
     .then(result => res.send(result.rows[0]))
+    .catch(console.error);
+})
+
+app.post('/api/v1/books', (req, res) => {
+  let SQL = 'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);'
+  let values = [
+    req.body.title,
+    req.body.author,
+    req.body.isbn,
+    req.body.image_url,
+    req.body.description
+  ]
+  client.query(SQL, values)
     .catch(console.error);
 })
 
